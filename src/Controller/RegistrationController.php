0e05bbc42a10213bfee8f5e4dbfb100213c860ca
Alexis Rosin <rosinalexis@yahoo.fr>
2021-08-08 19:20:16 +0200

    regirection logged in user if trying to acesse register

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -79,9 +79,9 @@ class RegistrationController extends AbstractController
         try {
             $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
         } catch (VerifyEmailExceptionInterface $exception) {
-            $this->addFlash('verify_email_error', $exception->getReason());
+            $this->addFlash('error', $exception->getReason());
 
-            return $this->redirectToRoute('app_register');
+            return $this->redirectToRoute('app_home');
         }
 
 
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -15,7 +15,7 @@ class SecurityController extends AbstractController
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
          if ($this->getUser()) {
-             $this->addFlash('error','Already logged in!');
+             $this->addFlash('info','Already logged in!');
              return $this->redirectToRoute('app_home');
          }
 
